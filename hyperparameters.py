ceb632f24d46000e1dfbd1bdd41c5f658c2f463a
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-01-10 21:25:38 +0800

    Update hyperparameters.py

--- hyperparameters.py
+++ hyperparameters.py
@@ -4,3 +4,6 @@ N_WAY = 5  # Number of classes in a task
 N_SHOT = 5  # Number of images per class in the support set
 N_QUERY = 10  # Number of images per class in the query set
 N_EVALUATION_TASKS = 100
+
+N_TRAINING_EPISODES = 80000
+N_VALIDATION_TASKS = 100

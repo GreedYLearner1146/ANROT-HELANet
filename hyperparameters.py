0939381cbfc6c97b4f62d221b7557d3b53d5f9e1
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-01-10 21:12:43 +0800

    Create hyperparameters.py

---
+++ hyperparameters.py
@@ -0,0 +1,6 @@
+################ Load samplers and loaders code here. ####################################
+
+N_WAY = 5  # Number of classes in a task
+N_SHOT = 5  # Number of images per class in the support set
+N_QUERY = 10  # Number of images per class in the query set
+N_EVALUATION_TASKS = 100

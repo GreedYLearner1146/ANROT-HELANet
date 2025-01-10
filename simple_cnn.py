60c6806be9cfd291b5c279765304730dadee29e2
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-01-10 20:33:22 +0800

    Update simple_cnn.py

--- simple_cnn.py
+++ simple_cnn.py
@@ -4,13 +4,15 @@ from tensorflow.keras.datasets import mnist
 import numpy as np
 
 ######## Simple CNN that served as a basis for the adversarial samples via FGSM ############
+######################### On the meta-train dataset #######################################
+
 #initialize our optimizer and model
 print("[INFO] compiling model...")
 opt = Adam(1e-3)
-model = SimpleCNN.build(width=84, height=84, depth=3, classes=36)
+model = SimpleCNN.build(width=84, height=84, depth=3, classes=60)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
     metrics=["accuracy"])
-# train the simple CNN on MNIST
+# train the simple CNN 
 print("[INFO] training network...")
 model.fit(np.asarray(new_X_val), np.asarray(onehot_encodedval),
     #validation_data=(testX, testY),
@@ -29,7 +31,7 @@ print("[INFO] loss: {:.4f}, acc: {:.4f}\n".format(loss, acc))
 #print("[INFO] adversarial images *after* fine-tuning:")
 #print("[INFO] loss: {:.4f}, acc: {:.4f}".format(loss, acc))
 
-#################### generate a set of adversarial from our train set ##########################
+#################### Generate a set of adversarial from our train set ##########################
 print("[INFO] generating adversarial examples with FGSM...\n")
 (advX, advY) = next(generate_adversarial_batch(model, len(new_X_train),
     np.asarray(new_X_train), np.asarray(onehot_encoded), (84, 84, 3), eps=0.20))  # Changed from 0.05, 0.1, 0.15, 0.20, 0.25, 0.30?
@@ -40,3 +42,83 @@ print("[INFO] loss: {:.4f}, acc: {:.4f}\n".format(loss, acc))
 
 ###############################################################################################
 
+# do a final evaluation of the model on the adversarial images
+(loss, acc) = model.evaluate(x=advX, y=advY, verbose=0)
+print("[INFO] adversarial images *after* fine-tuning:")
+print("[INFO] loss: {:.4f}, acc: {:.4f}".format(loss, acc))
+
+##############################################################################################
+
+train_array_adv = []
+
+for A,B in zip(advX,onehot_encoder.inverse_transform(advY)):  # Need include inverse_transform.
+  train_array_adv.append((A,B))
+
+Adv_X_train = [x[0] for x in train_array_adv]
+Adv_y_train = [x[1] for x in train_array_adv]
+
+Adv_intytrain = []
+
+for z in Adv_y_train:
+     Adv_intytrain.append(int(z))
+
+######################### Do the same for meta-valid set ########################
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+import numpy as np
+
+#initialize our optimizer and model
+print("[INFO] compiling model...")
+opt = Adam(1e-3)
+model = SimpleCNN.build(width=84, height=84, depth=3, classes=36)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+    metrics=["accuracy"])
+# train the simple CNN 
+print("[INFO] training network...")
+model.fit(np.asarray(new_X_val), np.asarray(onehot_encodedval),
+    #validation_data=(testX, testY),
+    batch_size=64,
+    epochs=100,
+    verbose=1)
+
+################################ Fine-tuning ##############################################
+(loss, acc) = model.evaluate(x=np.asarray(new_X_val), y=np.asarray(onehot_encodedval), verbose=0)
+print("")
+print("[INFO] normal testing images *after* fine-tuning:")
+print("[INFO] loss: {:.4f}, acc: {:.4f}\n".format(loss, acc))
+# do a final evaluation of the model on the adversarial images
+#(loss, acc) = model.evaluate(x=advX, y=advY, verbose=0)
+#print("[INFO] adversarial images *after* fine-tuning:")
+#print("[INFO] loss: {:.4f}, acc: {:.4f}".format(loss, acc))
+
+
+################### Generate a set of adversarial from our valid set ######################
+print("[INFO] generating adversarial examples with FGSM...\n")
+(advXval, advYval) = next(generate_adversarial_batch(model, len(new_X_val),
+    np.asarray(new_X_val), np.asarray(onehot_encodedval), (84, 84, 3), eps=0.20))  # Changed from 0.05, 0.1, 0.15, 0.20, 0.25?
+# re-evaluate the model on the adversarial images
+(loss, acc) = model.evaluate(x=advXval, y=advYval, verbose=0)
+print("[INFO] adversarial testing images:")
+print("[INFO] loss: {:.4f}, acc: {:.4f}\n".format(loss, acc))
+
+########################################################################################################
+
+val_array_adv = []
+
+for A,B in zip(advXval,onehot_encoderval.inverse_transform(advYval)):  # Need include inverse_transform.
+  val_array_adv.append((A,B))
+
+Adv_X_val = [x[0] for x in val_array_adv]
+Adv_y_val= [x[1] for x in val_array_adv]
+
+print(np.shape(Adv_X_val))
+print(np.shape(Adv_y_val))
+
+Adv_intyval = []
+
+for z in Adv_y_val:
+     Adv_intyval.append(int(z))
+
+print(np.shape(Adv_intyval))

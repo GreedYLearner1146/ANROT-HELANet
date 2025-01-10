8ae4c9dabb03ea8996e25bdb8ccdd0b102557860
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-01-11 02:54:07 +0800

    Create training.py

---
+++ training.py
@@ -0,0 +1,21 @@
+################################### Training of model ##################################
+
+log_update_frequency = 10
+
+all_loss = []
+model.train()
+with tqdm(enumerate(train_loader), total=len(train_loader)) as tqdm_train:
+    for episode_index, (
+        support_images,
+        support_labels,
+        query_images,
+        query_labels,
+        _,
+    ) in tqdm_train:
+        loss_value = fit(support_images.float(), support_labels, query_images.float(), query_labels)
+        all_loss.append(loss_value)
+
+        if episode_index % log_update_frequency == 0:
+            tqdm_train.set_postfix(loss=sliding_average(all_loss, log_update_frequency))
+
+# start at 45.

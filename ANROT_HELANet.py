90b7b273b426c0ccc8f7501bd8419b6fa75e5fdd
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-01-10 21:01:01 +0800

    Create ANROT_HELANet.py

---
+++ ANROT_HELANet.py
@@ -0,0 +1,36 @@
+class ANROT_HELANet(nn.Module):
+    def __init__(self, backbone: nn.Module):
+        super(HELA_VFA, self).__init__()
+        self.backbone = backbone
+
+    def forward(
+        self,
+        support_images: torch.Tensor,
+        support_labels: torch.Tensor,
+        query_images: torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Predict query labels using labeled support images.
+        """
+        # Extract the features of support and query images
+        z_support = self.backbone.forward(support_images)
+        z_query = self.backbone.forward(query_images)
+
+        # Infer the number of different classes from the labels of the support set
+        n_way = len(torch.unique(support_labels))
+        # Prototype i is the mean of all instances of features corresponding to labels == i
+        z_proto = torch.cat(
+            [
+                z_support[torch.nonzero(support_labels == label)].mean(0)
+                for label in range(n_way)
+            ]
+        )
+        # Compute the HELLINGER distance from queries to prototypes
+        dists = Hellinger_dist(z_query, z_proto)
+
+        # And here is the super complicated operation to transform those distances into classification scores!
+        scores = -dists
+        return scores
+
+convolutional_network_output = resnet12()
+model = ANROT_HELANet(convolutional_network_output).cuda()

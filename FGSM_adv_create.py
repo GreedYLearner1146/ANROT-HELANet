1925c80801586f903928c200ca4d917160a2c631
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-01-14 01:39:57 +0800

    Update FGSM_adv_create.py

--- FGSM_adv_create.py
+++ FGSM_adv_create.py
@@ -6,7 +6,103 @@ import numpy as np
 ######## Simple CNN that served as a basis for the adversarial samples via FGSM ############
 ######################### On the meta-train dataset #######################################
 
-#initialize our optimizer and model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+
+class SimpleCNN:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape
+    ##################################################
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+    ##################################################
+        # first CONV => RELU => BN layer set
+        model.add(Conv2D(32, (3, 3), strides=(2, 2), padding="same",
+            input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+    ######################################################
+        # second CONV => RELU => BN layer set
+        model.add(Conv2D(64, (3, 3), strides=(2, 2), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+    #########################################################
+        # first (and only) set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(128))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+        # return the constructed network architecture
+        return model
+
+# import the necessary packages
+from tensorflow.keras.losses import MSE
+import tensorflow as tf
+
+def generate_image_adversary(model, image, label, eps=2 / 255.0):
+    # cast the image
+    image = tf.cast(image, tf.float32)
+    # record our gradients
+    with tf.GradientTape() as tape:
+        # explicitly indicate that our image should be tacked for
+        # gradient updates
+        tape.watch(image)
+        # use our model to make predictions on the input image and
+        # then compute the loss
+        pred = model(image)
+        loss = MSE(label, pred)  # Use MSE.
+    # calculate the gradients of loss with respect to the image, then
+    # compute the sign of the gradient
+    gradient = tape.gradient(loss, image)
+    signedGrad = tf.sign(gradient)
+    # construct the image adversary
+    adversary = (image + (signedGrad * eps)).numpy()  #FGSM.
+    # return the image adversary to the calling function
+    return adversary
+
+
+# import the necessary packages
+import numpy as np
+
+def generate_adversarial_batch(model, total, images, labels, dims,
+    eps=0.25):  # Change to 0.005?
+    # unpack the image dimensions into convenience variables
+    (h, w, c) = dims
+  # we're constructing a data generator here so we need to loop
+    # indefinitely
+    while True:
+        # initialize our perturbed images and labels
+        perturbImages = []
+        perturbLabels = []
+        # randomly sample indexes (without replacement) from the
+        # input data
+        idxs = np.random.choice(range(0, len(images)), size=total,replace=False)
+    # loop over the indexes
+        for i in idxs:
+            # grab the current image and label
+            image = images[i]
+            label = labels[i]
+            # generate an adversarial image
+            adversary = generate_image_adversary(model,
+                image.reshape(1, h, w, c), label, eps=eps)
+            # update our perturbed images and labels lists
+            perturbImages.append(adversary.reshape(h, w, c))
+            perturbLabels.append(label)
+        # yield the perturbed images and labels
+        yield (np.array(perturbImages), np.array(perturbLabels))
+
+################ initialize our optimizer and model ###############################
 print("[INFO] compiling model...")
 opt = Adam(1e-3)
 model = SimpleCNN.build(width=84, height=84, depth=3, classes=60)
